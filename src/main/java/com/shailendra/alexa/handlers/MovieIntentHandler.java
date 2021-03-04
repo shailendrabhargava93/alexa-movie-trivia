18995f66495000331dceef8f4a549a7c0566dc62
shailendra-bhargava <shailendra@nhancenow.com>
2021-03-04 10:35:08 +0530

    changes

--- src/main/java/com/shailendra/alexa/handlers/MovieIntentHandler.java
+++ src/main/java/com/shailendra/alexa/handlers/MovieIntentHandler.java
@@ -2,15 +2,21 @@ package com.shailendra.alexa.handlers;
 
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.impl.IntentRequestHandler;
 import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.request.RequestHelper;
+import com.shailendra.alexa.interceptors.response.LogResponseInterceptor;
 import com.shailendra.alexa.properties.MovieSearchUtils;
 
 public class MovieIntentHandler implements IntentRequestHandler {
-
+	
+	static final Logger logger = LogManager.getLogger(LogResponseInterceptor.class);
+	
 	@Override
 	public boolean canHandle(HandlerInput handlerInput, IntentRequest intentRequest) {
 		return intentRequest.getIntent().getName().equals("MovieIntentHandler");
@@ -18,7 +24,7 @@ public class MovieIntentHandler implements IntentRequestHandler {
 
 	@Override
 	public Optional<Response> handle(HandlerInput handlerInput, IntentRequest intentRequest) {
-
+		logger.info("INSIDE LAUNCH HANDLER");
 		RequestHelper requestHelper = RequestHelper.forHandlerInput(handlerInput);
 		Optional<String> slotValue = requestHelper.getSlotValue("movie_name");
 		String movieName = slotValue.map(t -> t).orElse(null);
